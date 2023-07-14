d1346880cbee0c7b23f1c88a4883fcc45a6c0cd7
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-07-14 13:57:19 -0400

    Update main.py

--- 141. Linked List Cycle/main.py
+++ 141. Linked List Cycle/main.py
@@ -1,1 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None or head.next is None:
+            return False
+
+        slow = head  # The slow pointer will move one step at a time
+        fast = head.next  # The fast pointer will move two steps at a time
+
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+
+        return True
