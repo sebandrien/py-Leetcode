6096ff91bb6de7e478821dadab3825baa2cd4a19
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-12-20 15:51:05 -0500

    Update main.py

--- 238. Product of Array Except Self/main.py
+++ 238. Product of Array Except Self/main.py
@@ -1,23 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # length of the input list
-        length = len(nums)
-        
-        # The answer array to be returned
-        answer = [0]*length
-        
-        # answer[i] is initially the product of all numbers before i
-        answer[0] = 1
-        for i in range(1, length):
-            answer[i] = nums[i - 1] * answer[i - 1]
-        
-        # R contains the product of all numbers after i
-        R = 1;
-        for i in reversed(range(length)):
-            # For the index 'i', answer[i] contains the product of all numbers before i
-            # R contains the product of all numbers after i
-            answer[i] = answer[i] * R
-            # R now contains the product of all numbers after i+1
-            R *= nums[i]
-        
-        return answer
+        res = [1] * (len(nums))
+        prefix = 1
+        postfix = 1
+
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        for i in range(len(nums) -1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
