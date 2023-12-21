e9226c4e90837d6cab0998cdec037b8879dd5334
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-12-20 23:46:22 -0500

    Create main.py

---
+++ 211. Design Add and Search Words Data Structure/main.py
@@ -0,0 +1,12 @@
+class TrieNode
+  def __init__(self):
+    self.children = {}
+    self.word = False
+
+class WordDictionary:
+  def __init__(self):
+    self.root = TrieNode()
+
+  def addWord(self, x)
+
+  
